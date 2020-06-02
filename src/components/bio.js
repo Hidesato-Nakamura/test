907e529ba2791cfc36902bab432575cbca3d885c
株式会社アフェクション <affexion@kabushikikaishaafekushonnoMacBook-Pro.local>
2020-06-02 14:09:04 +0900

    a

--- src/components/bio.js
+++ src/components/bio.js
@@ -6,7 +6,8 @@
  */
 
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql 
+} from "gatsby"
 import Image from "gatsby-image"
 
 import { rhythm } from "../utils/typography"
