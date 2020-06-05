c373f972b5372e797c29f7719c3c050a5b95acae
株式会社アフェクション <affexion@kabushikikaishaafekushonnoMacBook-Pro.local>
2020-06-05 18:21:38 +0900

    window

--- src/components/categoryCard.js
+++ src/components/categoryCard.js
@@ -1,6 +1,9 @@
 import React from "react"
+import { kebabCase } from "lodash"
+import { Link } from "gatsby"
 
 const CategoryCard = () => {
+  let tags = ["aaa", "bbb", "ccc"]
   const categoryCard = (
     <section className="category-card">
       <p
@@ -9,6 +12,15 @@ const CategoryCard = () => {
         カテゴリー
       </p>
       <div>tags</div>
+      <ul>
+        {tags.map(tag =>
+          tags && tags.length ? (
+            <li key={tag + `tag`}>
+              <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+            </li>
+          ) : null
+        )}
+      </ul>
     </section>
   )
   return categoryCard
