b26ed9ddd4a7b8cb0f4aa263ce72d51d85d079e7
株式会社アフェクション <affexion@kabushikikaishaafekushonnoMacBook-Pro.local>
2020-06-10 07:52:19 +0900

    title

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `This is IT`,
+    title: `dummy`,
     author: {
       name: `テスト`,
       summary: `who lives and works in Tomioka city building useful things.`,
